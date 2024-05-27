cf3e89398a7c2031495cdfa7abbafc0b158e7141
Andy-Ibrahim <andyofcmusic@gmail.com>
2024-05-27 23:35:43 +1000

    Update script.py

--- Practical Project/Python Script/script.py
+++ Practical Project/Python Script/script.py
@@ -4,7 +4,7 @@ import mysql.connector
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="Hahalollmao32",
+    password="12345",
     database="iot_data"
 )
 cursor = mydb.cursor()
